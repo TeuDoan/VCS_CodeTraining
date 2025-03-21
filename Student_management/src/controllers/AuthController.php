bd5b10af81c42819e78b0a3f1ab007a3eccf81ba
RandomTeuz <teudoan@gmail.com>
2025-03-21 18:19:04 +0700

    Delete Student_management directory

--- Student_management/src/controllers/AuthController.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require_once __DIR__ . '/../../config/config.php'; // Include database connection
-require_once __DIR__ . '/../../session/session.php';
-require_once __DIR__ . '/../../models/User.php';
